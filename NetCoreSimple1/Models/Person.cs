7a260aa5fc6ad2ff059e5a2e9c693d0e6a793451
Danilov <albert95danilov@gmail.com>
2023-08-20 18:58:11 +0300

    API

---
+++ NetCoreSimple1/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreSimple1.Models
+{
+    public class Person
+    {
+        public string Id { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+        public int Age { get; set; }
+    }
+}

926453dc1eb8ac5911513e715df046dc75efd9db
Danilov <albert95danilov@gmail.com>
2023-08-27 14:37:29 +0300

    конвеер обработки запроса

---
+++ NetCoreSimple1/Models/RoutingMIddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace NetCoreSimple1.Models
+{
+    public class RoutingMIddleware
+    {
+        public RoutingMIddleware(RequestDelegate _) 
+        { 
+            
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path;
+            if (path == "/index")
+            {
+                await context.Response.WriteAsync("Home Page");
+            }
+            else if (path == "/about") 
+            {
+                await context.Response.WriteAsync("About Page");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        }
+    }
+}

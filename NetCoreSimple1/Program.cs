85896abd9e23bb09797a3515d03e8832388fbf8e
Danilov <albert95danilov@gmail.com>
2023-09-05 18:20:45 +0300

    Rout constraints

--- NetCoreSimple1/Program.cs
+++ NetCoreSimple1/Program.cs
@@ -11,37 +11,27 @@ using System.Text.RegularExpressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddTimeService();
-
-//builder.Services.AddSingleton<ICounter, RandomCounter>();
-//builder.Services.AddSingleton<CounterService>();
-
-//builder.Services.AddScoped<ITimer, NetCoreSimple1.Models.Timer>();
-//builder.Services.AddSingleton<TimeService>();
-
-builder.Services.AddTransient<IHelloService, RuHelloService>();
-builder.Services.AddTransient<IHelloService, EnHelloService>();
-
 var app = builder.Build();
 
-app.UseMiddleware<HelloMiddleware>();
+app.Map("/", () => "Index Page");
+app.Map("/users/{id}", (string id) => $"User id: {id}");
+app.Map("/users/{id:int}/{name:minlength(5):maxlength(150)}", HandleRequest);
+app.Map("/users/{id}-{name}", (string id, string name) => $"User id: {id}, name: {name}");
+app.Map("/about", () => "About Page");
+app.Map("/contact", () => "Contact Page");
+app.Map("/user", () => Console.WriteLine("Request Path: /user"));
+app.Map("/test", async (context) =>
+{
+    await context.Response.WriteAsync("Test page");
+});
 
-//app.UseMiddleware<TimerMiddleware>();
-
-//Console.WriteLine(app.Environment.EnvironmentName);
-
-//app.UseMiddleware<CounterMiddleware>();
-
-//app.UseMiddleware<ErrorHandlingMiddleware>();
-//app.UseMiddleware<AuthenticationMiddleware>();
-//app.UseMiddleware<RoutingMIddleware>();
-
-//app.Run(async context =>
-//{
-//    var timeService = app.Services.GetService<ITimeService>();
-//    await context.Response.WriteAsync($"Time: {timeService?.GetTime()}");
-//});
+app.MapGet("/", (IEnumerable<EndpointDataSource> endpointSources) => string.Join("\n", endpointSources.SelectMany(source => source.Endpoints)));
 
 app.Run();
+
+string HandleRequest(string id, string name)
+{
+    return $"User id: {id}, name: {name}";
+}
 
 
